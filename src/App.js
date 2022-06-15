26ea9c4e87b2ea0341532e370f9267528f711ca0
Alapottra <alapottratrojon@gmail.com>
2022-06-15 14:04:35 +0600

    basic template done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  
+  return (
+    <div>
+ 
+    <Navbar/>
+    <Routes>
+      
+
+    </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
